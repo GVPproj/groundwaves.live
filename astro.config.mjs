a066ab05e5034d0312b2b0e9215bc786497f722d
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-05-13 14:47:52 -0700

    feat: further og madness

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://groundwaves.live",
   output: "static",
   integrations: [
     tailwind({
