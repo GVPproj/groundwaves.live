78c59ca713187298081fbce7307c4070e6c1e701
Graham Van Pelt <grahamvanpelt@gmail.com>
2025-12-03 12:29:33 -0800

    feat: add RK and CG pics

--- src/data/afwContent.ts
+++ src/data/afwContent.ts
@@ -39,9 +39,9 @@ export const afwContent: AfwContent = {
         days: [
           {
             date: "Friday, Dec 5",
-            // images: [
-            //   "src/assets/afwAssets/new/AFW_002_Profile_Meredith-Bates_1.0.png",
-            // ],
+            images: [
+              "src/assets/afwAssets/new/AFW_002_Profile_Robert-Kraft_1.0.png",
+            ],
             events: [
               {
                 type: "Community Curation",
@@ -59,9 +59,9 @@ export const afwContent: AfwContent = {
           {
             date: "Saturday, Dec 6",
             images: [
+              "src/assets/afwAssets/new/AFW_002_Profile_Chris-Gestrin_1.0.png",
               "src/assets/afwAssets/new/AFW_002_Profile_Eddie-C_1.0.png",
               "src/assets/afwAssets/new/AFW_002_Profile_Chris_Arnett_1.1.png",
-              // "src/assets/afwAssets/new/AFW_002_Profile_Rave-Coach_1.0.png",
               // "src/assets/afwAssets/new/AFW_002_Profile_Kozue_1.3.png",
             ],
             intro:
@@ -129,7 +129,8 @@ export const afwContent: AfwContent = {
                   {
                     host: "Matt Roberson & Tea Mistress",
                     albums: ["Matt Robertson - Aequora"],
-                    description: "This Sunday, we invite you into a meditative journey through Aequora (a calm sea), an album painstakingly crafted from hundreds of hand-layered sine waves. What began as an exploration of additive synthesis—shaping music entirely from pure frequencies—evolved into something quietly extraordinary: seven tracks woven into one continuous sonic experience, designed to be heard exactly as you'll experience it here.\n\nThese soundscapes are calm, patient, and quietly alive. \n\nTea Mistress @teamistress will guide you through a flight of exquisite artisan teas, each one chosen to complement and elevate your journey through sound.\n\nBring pillows, blankets, whatever you need to settle in. The lights will be low. Together, we'll dedicate this time to the practice of listening—deeply and collectively.",
+                    description:
+                      "This Sunday, we invite you into a meditative journey through Aequora (a calm sea), an album painstakingly crafted from hundreds of hand-layered sine waves. What began as an exploration of additive synthesis—shaping music entirely from pure frequencies—evolved into something quietly extraordinary: seven tracks woven into one continuous sonic experience, designed to be heard exactly as you'll experience it here. \n\nThese soundscapes are calm, patient, and quietly alive. \n\nTea Mistress @teamistress will guide you through a flight of exquisite artisan teas, each one chosen to complement and elevate your journey through sound.\n\nBring pillows, blankets, whatever you need to settle in. The lights will be low. Together, we'll dedicate this time to the practice of listening—deeply and collectively.",
                   },
                 ],
               },
